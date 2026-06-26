4339a1bbb2734ebbe2a0a4a80ce12e85deee267e
MisaelSalazar <MisaelSalazarOf@protonmail.com>
2026-06-25 19:38:35 -0600

    Primera version de la app

---
+++ src/main/java/com/mycompany/novedadessary/NovedadesSary.java
@@ -0,0 +1,32 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.novedadessary;
+
+import com.formdev.flatlaf.FlatLightLaf;
+import com.mycompany.novedadessary.Views.Login;
+import javax.swing.SwingUtilities;
+import javax.swing.UIManager;
+import javax.swing.UnsupportedLookAndFeelException;
+
+/**
+ *
+ * @author Misae
+ */
+public class NovedadesSary {
+
+    public static void main(String[] args) {
+        //System.out.println("Hello World!");
+        try {
+            // Estilo tipo Cupertino Light
+            UIManager.setLookAndFeel(new FlatLightLaf());
+        } catch (UnsupportedLookAndFeelException ex) {
+            System.err.println("No se pudo cargar FlatLaf");
+        }
+
+        SwingUtilities.invokeLater(() -> {
+            new Login().setVisible(true);
+        });
+    }
+}
